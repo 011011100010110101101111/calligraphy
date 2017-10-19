4329d3ec6f04daafba6e9d984fd463df9a789ed0
gitors <1@gitor.org>
2017-10-19 09:15:12 +0800

    test

--- src/main/java/com/hpwenxue/domain/Tags.java
+++ src/main/java/com/hpwenxue/domain/Tags.java
@@ -1,9 +1,54 @@
 package com.hpwenxue.domain;
+
+import java.util.Date;
+
 /**
  * 标签
  * @author allen
  *
  */
 public class Tags {
+	private Integer id;
+	private String name;
+	private Date createTime;
+	private Date updateTime;
+	
+	@Override
+	public String toString() {
+		return "Tags [id=" + id + ", name=" + name + ", createTime=" + createTime + ", updateTime=" + updateTime + "]";
+	}
 
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getCreateTime() {
+		return createTime;
+	}
+	
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+	
 }
