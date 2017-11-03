04cdc72f4e07c22cd3f6126591d2548448d4c386
coderlwl <1@gitor.org>
2017-11-03 23:47:45 +0800

    :art: 主要代码增加

---
+++ src/main/java/com/hpwenxue/service/CommentsService.java
@@ -0,0 +1,10 @@
+package com.hpwenxue.service;
+/**
+ * 评论service
+ * @author allen
+ * @website gitor.org
+ * @date 2017年11月3日下午11:47:28
+ */
+public interface CommentsService {
+
+}
---
+++ src/main/java/com/hpwenxue/service/impl/CommentsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hpwenxue.service.impl;
+
+import com.hpwenxue.service.CommentsService;
+/**
+ * 
+ * @author allen
+ * @website gitor.org
+ * @date 2017年11月3日下午11:47:37
+ */
+public class CommentsServiceImpl implements CommentsService {
+
+}
