dac33e7475c091560ef2a595c13506a5675dd5c8
coderlwl <1@gitor.org>
2017-11-03 22:21:42 +0800

    s

---
+++ src/main/java/com/hpwenxue/service/ArticlesService.java
@@ -0,0 +1,9 @@
+package com.hpwenxue.service;
+/**
+ * 文章管理接口
+ * @author allen
+ *
+ */
+public interface ArticlesService {
+
+}
---
+++ src/main/java/com/hpwenxue/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,14 @@
+package com.hpwenxue.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.hpwenxue.service.ArticlesService;
+/**
+ * 文章管理接口实现类
+ * @author allen
+ *
+ */
+@Service("articleServiceImpl")
+public class ArticleServiceImpl implements ArticlesService {
+
+}
