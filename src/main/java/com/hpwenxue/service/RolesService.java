a9d6cf43f00e3a25841bde1a9306c8c3b1646392
coderlwl <1@gitor.org>
2017-11-06 23:37:06 +0800

    !

--- src/main/java/com/hpwenxue/service/RolesService.java
+++ src/main/java/com/hpwenxue/service/RolesService.java
@@ -1,5 +1,16 @@
 package com.hpwenxue.service;
 
-public interface RolesService {
+import java.util.List;
 
+import com.hpwenxue.domain.Roles;
+
+public interface RolesService {
+	public Roles getOne(Integer id);
+	public List<Roles> getAll();
+	public void save(Roles roles);
+	
+	public void delete(Integer id);
+	
+	public void deleteBatch(List<Integer> ids);
+	
 }
