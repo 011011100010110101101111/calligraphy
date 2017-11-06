ba215c21e3fa1ef7c5b1900dabf2de53beeed1fe
coderlwl <1@gitor.org>
2017-11-06 23:39:06 +0800

    ...

---
+++ src/main/java/com/hpwenxue/service/impl/RolesServiceImpl.java
@@ -0,0 +1,40 @@
+package com.hpwenxue.service.impl;
+
+import java.util.List;
+
+import com.hpwenxue.domain.Roles;
+import com.hpwenxue.service.RolesService;
+
+public class RolesServiceImpl implements RolesService {
+
+	@Override
+	public Roles getOne(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Roles> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Roles roles) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteBatch(List<Integer> ids) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/hpwenxue/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.hpwenxue.service.impl;
+
+import com.hpwenxue.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
