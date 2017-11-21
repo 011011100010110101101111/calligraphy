fc56253ba2bd07dba4ebe5500f8565f91178f032
刘文龙 <1@gitor.org>
2017-11-21 14:46:53 +0800

    :art:

---
+++ src/main/java/com/hpwenxue/domain/dto/ArticlesDto.java
@@ -0,0 +1,18 @@
+package com.hpwenxue.domain.dto;
+/**
+ * 文章dto 传输对象
+ * @author liuwenlong
+ * @date 2017年11月21日下午2:45:22
+ */
+public class ArticlesDto {
+	private String a ;
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+	
+}
