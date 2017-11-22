deaa9ab01c527d7189e125323ae42eaa11ef3c05
刘文龙 <1@gitor.org>
2017-11-22 15:06:35 +0800

    2

---
+++ src/main/java/com/hpwenxue/service/LoginService.java
@@ -0,0 +1,12 @@
+package com.hpwenxue.service;
+
+import java.util.Map;
+
+public interface LoginService {
+	/**
+	 * 登陆
+	 * @param parmas
+	 * @return
+	 */
+	public Map<String,Object> login(Map<String,Object> parmas);
+}
