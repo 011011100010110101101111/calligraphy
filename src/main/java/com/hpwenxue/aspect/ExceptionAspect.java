8d8aa4f88b9ff4f466dcbd946214943296a46a70
刘文龙 <1@gitor.org>
2017-12-23 21:31:49 +0800

    处理异常信息

--- src/main/java/com/hpwenxue/aspect/ExceptionAspect.java
+++ src/main/java/com/hpwenxue/aspect/ExceptionAspect.java
@@ -1,9 +1,51 @@
 package com.hpwenxue.aspect;
+
+import com.hpwenxue.utils.E;
+import com.hpwenxue.utils.R;
+import org.apache.shiro.authz.AuthorizationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 /**
  * 统一异常处理
- * @author Administrator
+ * @author liuwenlong
  *
  */
+@RestControllerAdvice
 public class ExceptionAspect  {
-	
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 处理自定义异常
+     */
+    @ExceptionHandler(E.class)
+    public R handleRRException(E e){
+        R r = new R();
+        r.put("code", e.getCode());
+        r.put("msg", e.getMessage());
+
+        return r;
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public R handleDuplicateKeyException(DuplicateKeyException e){
+        logger.error(e.getMessage(), e);
+        return R.error("数据库中已存在该记录");
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public R handleAuthorizationException(AuthorizationException e){
+        logger.error(e.getMessage(), e);
+        return R.error("没有权限，请联系管理员授权");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e){
+        logger.error(e.getMessage(), e);
+        return R.error();
+    }
 }
--- src/main/java/com/hpwenxue/utils/E.java
+++ src/main/java/com/hpwenxue/utils/E.java
@@ -4,14 +4,14 @@ package com.hpwenxue.utils;
  * @author allen
  *
  */
-public class E extends Exception{
+public class E extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	private Integer code;
 	private String message;
 	
 	public E(){
-		this.code = 0;
-		this.message = "";
+		this.code = 500;
+		this.message = "未知的异常，请联系管理员！";
 	}
 	public E(Integer code,String message){
 		this.code = code;
@@ -24,5 +24,21 @@ public class E extends Exception{
 	public static E cNn(){
 		return new E();
 	}
-	
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
