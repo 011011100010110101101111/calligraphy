89598dcd98c6b7054ee85474f3c2ba3f8b50dc18
刘文龙 <1@gitor.org>
2018-01-13 16:22:44 +0800

    新增代码，在系统启动的时候，初始化系统数据

--- src/main/resources/doc/db/literrature.sql
+++ src/main/resources/doc/db/literrature.sql
@@ -4,3 +4,5 @@ Database scripts, due to the original system data, so there will be some SQL, fo
  数据库脚本，由于需要原始系统数据，所以会有一些sql，后续考虑 系统初始化的时候自动创建表结构 外初始化系统数据，前期设计先考虑到这个吧
 
 */
+
+INSERT INTO user (age, pass_word, real_name, sex, user_name, photograph) VALUES (25,'lwl123','刘文龙',1,'admin','sssss');
