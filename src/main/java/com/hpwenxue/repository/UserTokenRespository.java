042291cba9c05139619d7b3d38ce40bed761dd91
刘文龙 <1@gitor.org>
2018-01-31 14:46:10 +0800

    usertoken

--- src/main/java/com/hpwenxue/repository/UserTokenRespository.java
+++ src/main/java/com/hpwenxue/repository/UserTokenRespository.java
@@ -1,1 +1,1 @@
-package com.hpwenxue.repository;/** * @author : liuwenlong * @date : 2018/1/25 11:17 */public interface UserTokenRespository {}
+package com.hpwenxue.repository;import com.hpwenxue.domain.UserToken;import org.springframework.data.jpa.repository.JpaRepository;/** * UserToken Respository * @author : liuwenlong * @date : 2018/1/25 11:17 */public interface UserTokenRespository extends JpaRepository<UserToken,Integer> {}
