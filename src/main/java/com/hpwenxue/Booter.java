2d880c37c3ad120594d9ee5d550945b2b80729da
gitor <1@gitor.org>
2018-06-09 19:17:09 +0800

    3

--- src/main/java/com/hpwenxue/Booter.java
+++ src/main/java/com/hpwenxue/Booter.java
@@ -1,12 +1,24 @@
 package com.hpwenxue;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class Booter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Booter.class, args);
+		SpringApplication app = new SpringApplication(Booter.class);
+		app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                
+            }
+        });
+		app.run(args);
+		//SpringApplication.run(Booter.class, args);
 	}
 }
